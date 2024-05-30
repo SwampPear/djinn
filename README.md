04878d31464028bffea644090144868618b1d761
SwampPear <michaeljosephvaden03@gmail.com>
2024-05-29 22:26:50 -0400

    implemented skeletons for commands

---
+++ README.md
@@ -0,0 +1,3 @@
+prompt: write me a program that adds two numbers in python
+load
+output:
--- conf.py
+++ conf.py
@@ -2,14 +2,5 @@ import os
 import sys
 
 
-"""
-TODO: Dynamic platforming
-
-if sys.platform == 'nt':
-    PLATFORM = 'windows'
-else:               
-    PLATFORM = 'unix'
-"""
-
 PROJECT_NAME = 'workspace'
-ENVIRONMENT = f'{os.getcwd()}\{PROJECT_NAME}'
+ENVIRONMENT = f'{os.getcwd()}/{PROJECT_NAME}'
