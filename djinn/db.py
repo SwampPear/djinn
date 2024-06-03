0c2d8d4bf6fa07825dc46c72d6c8d9101a62c54f
SwampPear <michaeljosephvaden03@gmail.com>
2024-06-02 20:23:04 -0400

    implemented db querying and script to create log table

--- djinn/db.py
+++ djinn/db.py
@@ -1,6 +1,21 @@
+import sqlite3
+
+
 class Database:
     """
     Database interface.
     """
     def __init__(self, db_path: str) -> None:
         self.db_path = db_path
+
+        self.conn = sqlite3.connect(self.db_path)
+
+
+    """
+    Querys the database.
+    """
+    def query(self, sql: str) -> None:
+        cursor = self.conn.cursor()
+        cursor.execute(sql)
+
+        self.conn.commit()
--- scripts/create_log_table.py
+++ scripts/create_log_table.py
@@ -1,6 +1,19 @@
-def create_log_table() -> None:
-    pass
+from djinn.db import Database
 
+
+def create_log_table() -> None:
+    db = Database('data.db')
+
+    sql = """
+    CREATE TABLE IF NOT EXISTS log (
+        id INTEGER PRIMARY KEY AUTOINC NOT NULL,
+        type TEXT NOT NULL,
+        type TEXT NOT NULL
+    );
+    """
+
+    db.query(sql)
+    
 
 if __name__ == '__main__':
     create_log_table()
