dea666d40785b8c7740405e3e03b4bbcad7f8152
SwampPear <michaeljosephvaden03@gmail.com>
2024-07-01 19:30:18 -0400

    moved temp back to normal

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
+import sys
+from djinn import Controller
 
 
-app = Flask(__name__)
+if __name__ == '__main__':
+    prompt = sys.argv[1]
 
-@app.route('/')
-def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+    controller = Controller()
 
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    controller.run()
