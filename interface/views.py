c394dbc35b9ce044ae46cbaf67e118056e378bf4
SwampPear <michaeljosephvaden03@gmail.com>
2024-07-02 13:17:15 -0400

    building view for crud api

--- interface/views.py
+++ interface/views.py
@@ -1,8 +1,18 @@
-from flask import render_template, Blueprint
+from flask import Blueprint, request
 
 
+# log
 log_view = Blueprint('log_view', __name__)
 
 @log_view.route('/api/v1/log')
 def log():
-    return {'apple': 3}
+    if request.method == 'POST':
+        return 'post'
+    elif request.method == 'GET':
+        return 'get'
+    elif request.method == 'PATCH':
+        return 'patch'
+    elif request.method == 'DELETE':
+        return 'delete'
+    else:
+        return 'request method not accepted'
