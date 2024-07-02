dc5cb2390387a029543d2014c9cd2375a411af12
SwampPear <michaeljosephvaden03@gmail.com>
2024-07-02 13:20:20 -0400

    set up scaffolding for api objects'

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from interface import log_view
+from interface import project_view, sprint_view, iteration_view, log_view
 
 
 app = Flask(
@@ -7,12 +7,15 @@ app = Flask(
     template_folder='/app/interface/templates',
     static_folder='/app/interface/static')
 
+app.register_blueprint(project_view)
+app.register_blueprint(sprint_view)
+app.register_blueprint(iteration_view)
 app.register_blueprint(log_view)
 
 @app.route('/')
-def hello_geek():
+def index():
     return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=False, )
+    app.run(debug=False)
--- interface/views.py
+++ interface/views.py
@@ -1,5 +1,55 @@
 from flask import Blueprint, request
 
+# project
+project_view = Blueprint('project_view', __name__)
+
+@project_view.route('/api/v1/project')
+def project():
+    if request.method == 'POST':
+        return 'post'
+    elif request.method == 'GET':
+        return 'get'
+    elif request.method == 'PATCH':
+        return 'patch'
+    elif request.method == 'DELETE':
+        return 'delete'
+    else:
+        return 'request method not accepted'
+    
+
+# sprint
+sprint_view = Blueprint('sprint_view', __name__)
+
+@sprint_view.route('/api/v1/sprint')
+def sprint():
+    if request.method == 'POST':
+        return 'post'
+    elif request.method == 'GET':
+        return 'get'
+    elif request.method == 'PATCH':
+        return 'patch'
+    elif request.method == 'DELETE':
+        return 'delete'
+    else:
+        return 'request method not accepted'
+    
+
+# iteration
+iteration_view = Blueprint('iteration_view', __name__)
+
+@sprint_view.route('/api/v1/iteration')
+def iteration():
+    if request.method == 'POST':
+        return 'post'
+    elif request.method == 'GET':
+        return 'get'
+    elif request.method == 'PATCH':
+        return 'patch'
+    elif request.method == 'DELETE':
+        return 'delete'
+    else:
+        return 'request method not accepted'
+
 
 # log
 log_view = Blueprint('log_view', __name__)
