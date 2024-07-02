bbe610089a69093dcb8b6c02330d051f5e95c109
SwampPear <michaeljosephvaden03@gmail.com>
2024-07-01 21:42:55 -0400

    attempting to fix flask template error

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 import os
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='/app/interface/templates')
 
 @app.route('/')
 def hello_geek():
-    return render_template('/app/interface/templates/index.html')
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False, )
