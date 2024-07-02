9ac6870f89c7cfdc0525d5b0cc7d0b324f5ee506
SwampPear <michaeljosephvaden03@gmail.com>
2024-07-01 20:13:41 -0400

    should work

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return render_template('interface/templates/index.html')
+    return render_template('/app/interface/templates/index.html')
 
 
 if __name__ == "__main__":
-    print(os.getcwd())
-    #app.run(debug=False)
+    app.run(debug=False)
