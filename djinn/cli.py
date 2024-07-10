cb069e70b7026db8c2f81f91e51ee12571e5ff17
Vaden <michael.vaden@delta.com>
2024-07-10 10:28:50 -0400

    routed new, start, and rm commands for cli'

--- djinn/cli.py
+++ djinn/cli.py
@@ -4,35 +4,67 @@ import os
 
 class CLI:
     def __init__(self):
-        # init parser
-        self.parser = ArgumentParser(description='Djinn CLI')
+        self.parser = self._init_parser()
 
-        # init commands
+
+    def _init_parser(self):
+        parser = ArgumentParser(description='Djinn CLI')
+
         # command
-        self.parser.add_argument(
-            'command', 
-            choices=['start'], 
-            default='start',
+        parser.add_argument(
+            'command',
+            choices=[
+                'new',
+                'start',
+                'rm'
+            ], 
             help='basic command for djinn')
 
+        # project
+        parser.add_argument(
+            'project',
+            help='name of a project'
+        )
+
         # workspace
-        self.parser.add_argument(
-            '--workspace', '-o',
+        parser.add_argument(
+            '--workspace', '-w',
             default=os.getcwd(),
             required=False,
-            help='adds a workspace dir')
+            help='workspace dir')
+
+        return parser
+
+
+    def _new(self, project, workspace):
+        print('new')
+        print(project)
+        print(workspace)
+
+
+    def _start(self, project, workspace):
+        print('start')
+        print(project)
+        print(workspace)
+
+
+    def _rm(self, project, workspace):
+        print('rm')
+        print(project)
+        print(workspace)
         
-        # project name
-        self.parser.add_argument(
-            '--project', '-p',
-            help='sets a project name')
         
     def run(self) -> None:
         # parse args
         args = self.parser.parse_args()
 
         command = args.command
+        project = args.project
         workspace = args.workspace
 
-        if command == 'start':
-            print(workspace)
+        if command == 'new':
+            self._new(project, workspace)
+        elif command == 'start':
+            self._start(project, workspace)
+        elif command == 'rm':
+            self._rm(project, workspace)
--- specifications.md
+++ specifications.md
@@ -9,8 +9,11 @@
 1. search to see if project name is in projects dir if it is, error
 2. initialize <project name>/data
 3. initialize <project name>/settings.json and populate
-4. open app
 
 # djinn start <project name>
 1. search to see if project name is in projects dir if not, error
 2. open app
+
+# djinn rm <project name>
+1. search to see if project name is in projects dir if not, error
+2. delete project folder from projects dir
