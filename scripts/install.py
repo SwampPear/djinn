41c9ecb12afc3c3307130612115b66cb1a66d460
Vaden <michael.vaden@delta.com>
2024-07-10 10:43:21 -0400

    enumerated installation process in new install.py

---
+++ scripts/install.py
@@ -0,0 +1,17 @@
+def main():
+    pass
+    # init lib dir (/Library/Djinn)
+
+    # copy startup script to lib dir (/Library/Djinn/djinn.py)
+
+    # init data dir (/var/db/Djinn)
+    # init user data dir (/var/db/Djinn/user)
+    # init user settings (/var/db/Djinn/user/settings.json)
+    # init project dir (/var/db/Djinn/projects)
+
+    # copy lib to lib dir (cp djinn/ /Library/Djinn)
+
+    # TODO: install pip requirements when necessary
+
+if __name__ == '__main__':
+    main()
