d7dc4b14653b1d446511b480df3a196099c9013a
SwampPear <michaeljosephvaden03@gmail.com>
2024-07-09 22:23:07 -0400

    script successfully initializes djinn

--- scripts/init_djinn.sh
+++ scripts/init_djinn.sh
@@ -6,6 +6,11 @@ sudo mkdir /Library/Djinn
 # init startup script
 sudo cp djinn.py /Library/Djinn/djinn.py
 
+# init data
+sudo mkdir /var/db/Djinn
+sudo rm /var/db/Djinn/data
+sudo touch /var/db/Djinn/data
+
 # copy lib contents
 sudo rm -rf /Library/Djinn/djinn
 sudo cp -r djinn/ /Library/Djinn/djinn
