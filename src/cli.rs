696ad212f8128a1ae0bb0ebf6c028efc0095caff
SwampPear <michaeljosephvaden03@gmail.com>
2024-09-09 13:43:47 -0400

    finished command line parsing and state serialization

--- src/cli.rs
+++ src/cli.rs
@@ -1,50 +1,114 @@
-use clap::{arg, command, Command};
+use clap::{command, Command, Arg};
+use std::env;
+use std::path::PathBuf;
+
+enum CLICommand {
+    NEW,
+    RM,
+    PROMPT
+}
+
+struct CLIArgs {
+    command: CLICommand,
+    project: String,
+    workspace: PathBuf,
+    prompt: String
+}
+
+impl CLIArgs {
+    pub fn new() -> Self {
+        CLIArgs {
+            command: CLICommand::NEW,
+            project: String::from(""),
+            workspace: PathBuf::new(),
+            prompt: String::from("")
+        }
+    }
+}
+
+fn cwd() -> PathBuf {
+    match env::current_dir() {
+        Ok(path) => path,
+        Err(_) => PathBuf::new()
+    }
+}
 
 pub fn parse_args() {
+    let mut cli_args = CLIArgs::new();
+
+    // command buildup
     let matches = command!()
         .subcommand(
             Command::new("new")
                 .about("creates a new Djinn project")
-                .arg(arg!([project] "project name"))
+                .arg(
+                    Arg::new("project")
+                    .value_name("PROJECT")
+                    .help("Specify PROJECT to create")
+                )
+                .arg(
+                    Arg::new("workspace")
+                    .value_name("WORKSPACE")
+                    .short('w')
+                    .long("workspace")
+                    .help("Specify WORKSPACE directory to initialize")
+                )
         )
         .subcommand(
             Command::new("rm")
                 .about("removes a Djinn project")
-                .arg(arg!([project] "project name"))
+                .arg(
+                    Arg::new("project")
+                    .value_name("PROJECT")
+                    .help("Specify PROJECT to remove")
+                )
         )
         .subcommand(
             Command::new("prompt")
                 .about("creates a new djinn project")
-                .arg(arg!([project] "project name"))
-                .arg(arg!([prompt] "prompt"))
+                .arg(
+                    Arg::new("project")
+                    .value_name("PROJECT")
+                    .help("Specify PROJECT to remove")
+                )
+                .arg(
+                    Arg::new("prompt")
+                    .value_name("PROMPT")
+                    .help("Specify PROMPT to act on")
+                )
         )
         .get_matches();
 
+    // command serialization
     if let Some(matches) = matches.subcommand_matches("new") {
+        cli_args.command = CLICommand::NEW;
+
         if let Some(project) = matches.get_one::<String>("project") {
-            println!("Value for name: {project}");
+            cli_args.project = project.to_string();
+        }
+
+        if let Some(workspace) = matches.get_one::<String>("workspace") {
+            cli_args.workspace = workspace.to_string().into();
         } else {
-            println!("nah project");
+            cli_args.workspace = cwd();
         }
     } else
     if let Some(matches) = matches.subcommand_matches("rm") {
+        cli_args.command = CLICommand::RM;
+
         if let Some(project) = matches.get_one::<String>("project") {
-            println!("Value for name: {project}");
-        } else {
-            println!("nah project");
+            cli_args.project = project.to_string();
         }
     } else
     if let Some(matches) = matches.subcommand_matches("prompt") {
+        cli_args.command = CLICommand::PROMPT;
+
         if let Some(project) = matches.get_one::<String>("project") {
-            println!("Value for name: {project}");
-        } else {
-            println!("nah project");
+            cli_args.project = project.to_string();
         }
 
         if let Some(prompt) = matches.get_one::<String>("prompt") {
-            println!("Value for name: {prompt}");
-        } else {
-            println!("nah prompt");
+            cli_args.prompt = prompt.to_string();
         }
     }
 }
