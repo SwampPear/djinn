8ebe741f2e6ec12f25e2ead26a54deadaf6a3a43
SwampPear <michaeljosephvaden03@gmail.com>
2024-09-22 11:15:25 -0400

    did some refactoring the rm and new commands

--- src/app.rs
+++ src/app.rs
@@ -18,50 +18,56 @@ impl Settings {
     }
 }
 
-fn new(args: cli::CLIArgs) {
+fn get_project_path(project: &String) -> PathBuf {
     let root = std::env::var("DJINN_ROOT")
         .expect("DJINN_ROOT environment variable must be set.");
 
     let path: PathBuf = [
-        root.clone(),
+        root,
         "projects".into(),
-        args.project.clone()
+        project.into()
     ]
     .iter()
     .collect();
 
+    return path;
+}
+
+fn get_project_subpath(project: &String, subpath: &str) -> PathBuf {
+    let root = std::env::var("DJINN_ROOT")
+        .expect("DJINN_ROOT environment variable must be set.");
+
+    let path: PathBuf = [
+        root,
+        "projects".into(),
+        project.into(),
+        subpath.into()
+    ]
+    .iter()
+    .collect();
+
+    return path;
+}
+
+fn new(args: cli::CLIArgs) {
+    let path: PathBuf = get_project_path(&args.project);
+
     if path.exists() {
-        println!("{}", "Project already exists");
+        println!("{}", "Project already exists.");
     } else {
         // create project directory
         fs::create_dir(path);
 
         // create data file
-        let data_path: PathBuf = [
-            root.clone(),
-            "projects".into(),
-            args.project.clone(),
-            "data".into()
-        ]
-        .iter()
-        .collect();
-
+        let data_path: PathBuf = get_project_subpath(&args.project, "data");
         fs::write(data_path, "");
 
         // create settings
-        let settings_path: PathBuf = [
-            root.clone(),
-            "projects".into(),
-            args.project.clone(),
-            "settings.json".into()
-        ]
-        .iter()
-        .collect();
-
+        let settings_path: PathBuf = get_project_subpath(&args.project, "settings.json");
         let settings = Settings::new(args.workspace);
 
+        // fmt json and write
         let j = serde_json::to_string(&settings).unwrap();
-
         fs::write(settings_path, j);
 
         println!("Project created successfully.")
@@ -69,19 +75,11 @@ fn new(args: cli::CLIArgs) {
 }
 
 fn rm(args: cli::CLIArgs) {
-    let root = std::env::var("DJINN_ROOT")
-        .expect("DJINN_ROOT environment variable must be set.");
-
-    let path: PathBuf = [
-        root,
-        "projects".into(),
-        args.project
-    ]
-    .iter()
-    .collect();
+    let path: PathBuf = get_project_path(&args.project);
 
     if path.exists() {
         fs::remove_dir(path);
+        println!("{}", "Project successfully removed.");
     } else {
         println!("{}", "Project does not exist.");
     }
